43c570205d4c5afb6eac3fe7a6c0a3831f0fb78c
github username <github email address>
2018-12-14 13:36:02 -0500

    fizzbuzz done

--- practice.rb
+++ practice.rb
@@ -66,3 +66,107 @@ puts hash
 end
 
 letter_counter
+
+
+# 4. given an index N find the value at N in the fibonacci sequence recursively
+# fib sequence: 1,1,2,3,5,8,13,21
+# indices: 1,2,3,4,5,6,7 ,8
+
+
+def fib(index)
+  if index == 1
+    return 1
+  elsif index == 0
+    return 0
+  else
+    return fib(index - 1) + fib(index - 2)
+  end
+end
+
+puts fib(5)
+
+
+# 5. given a number N where 1 billion > N > 0, n is a whole number
+# output N in words
+# ie: 3984756
+# output: three million nine hundred eighty four thousand seven hundred fifty six
+
+
+
+# def say_number(number)
+#   i = 0
+#   q = number / 1000
+#     while q > 0
+#       i ++
+#       q = q / 1000
+#   end
+# end
+#
+#  def units(index)
+#    units = {
+#     '5': 'million',
+#     '4': 'thousand',
+#     '3': 'hundred'
+#    }
+#   puts units[index.to_s]
+#  end
+#
+#  puts units(5)
+
+#
+#
+# def teens(ones)
+#   teens_map = {
+#     '0': 'ten',
+#     '1': 'eleven'
+#     ...
+#     '9': 'nineteen'
+#   }
+#   teens_map[ones.to_s]
+# end
+# say_number(1)
+
+
+
+# FizzBuzz
+#
+# The task is to print out the numbers 1 through 100, except in the following cases:
+#
+#   1. If the number is divisible by 3, print Fizz
+#
+#   2. If the number is divisible by 5, print Buzz
+#
+#   3. If the number is divisible by 3 and 5, print FizzBuzz
+#
+# You can write the solution in any language. Example output:
+#
+# 1
+# 2
+# Fizz
+# 4
+# Buzz
+# Fizz
+# 7
+# ...
+# 14
+# FizzBuzz
+# 16
+# ...
+#
+# Compare your results against `output.txt`.
+
+def fizzbuzz
+  (1..100).each do |number|
+    if number % 3 == 0
+      puts "Fizz"
+    elsif number % 5 == 0
+      puts "Buzz"
+    elsif number % 3 == 0 && number % 5 == 0
+      puts "FizzBuzz"
+    else
+    puts number
+    end
+  end
+end
+
+fizzbuzz
